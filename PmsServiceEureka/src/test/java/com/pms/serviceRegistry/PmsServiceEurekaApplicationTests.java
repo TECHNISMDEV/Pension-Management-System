2cecf6578b6861d7cba0e400f9ffa3a0e1920bd9
situ <situparna.sedge@gmail.com>
2022-07-22 01:34:48 +0530

    Eureka Server
    Eureka Server

---
+++ PmsServiceEureka/src/test/java/com/pms/serviceRegistry/PmsServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pms.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsServiceEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
